7de95d86b1259824f2a22c33643ab37fa5947c27
unknown <subisuresh69@gmail.com>
2025-01-16 23:30:13 +0530

    pgms added

--- csvop.py
+++ csvop.py
@@ -7,10 +7,12 @@ with open('csv1.csv','r') as csv1:
     for row in readerlist:
         dict1[row[0]]=len(row[0])
     print(dict1)
-    print(dict1.items())
+    dict2=dict(sorted(dict1.items(),key=lambda x: x[1]))
+    print(dict2)
     
     dict_list=[{'Name':row[0],'Length':len(row[0])} for row in readerlist]
     print(dict_list)    
+    
 
 with open('csv2.csv','a+') as csv2:
     writer=csv.DictWriter(csv2,fieldnames=['Name','Length'])
--- inherit.py
+++ inherit.py
@@ -1,25 +1,28 @@
 class Person:
-    def __init__(self,name,code):
-        self.name=name
-        self.code=code
+    def __init__(self, name, code):
+        self.name = name
+        self.code = code
+
 class Account(Person):
-    def __init__(self,name,code,pay):
-        Person.__init__(self,name,code)
-        self.pay=pay
+    def __init__(self, name, code, pay):
+        super().__init__(name, code)
+        self.pay = pay
+
 class Admin(Person): 
-    def __init__(self,name,code,exp):
-        Person.__init__(self,name,code)
-        self.exp=exp
-class Employee(Account,Admin):
-    def __init__(self,name,code,pay,exp):
-        Account.__init__(self,name,code,pay)
-        Admin.__init__(self,name,code,exp)
+    def __init__(self, name, code, exp):
+        super().__init__(name, code)
+        self.exp = exp
+
+class Employee(Account, Admin):
+    def __init__(self, name, code, pay, exp):
+        super().__init__(name, code, pay)
+        self.exp = exp
         
     def get_details(self):
-        print(f"Name : {self.name}")
-        print(f"Code : {self.code}")
-        print(f"pay : {self.pay}")
-        print(f"Exp : {self.exp}")
+        print(f"Name: {self.name}")
+        print(f"Code: {self.code}")
+        print(f"Pay: {self.pay}")
+        print(f"Experience: {self.exp}")
 
-e1=Employee("Subi",12345,50000,"2yrs")
+e1 = Employee("Subi", 12345, 50000, "2yrs")
 e1.get_details()
